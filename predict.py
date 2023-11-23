2a3669976726482b6f8db6ba5385f667ef42f5a5
Tamilselvan K <99823984+tzktz@users.noreply.github.com>
2023-11-23 17:26:28 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,80 +1,103 @@
+import os
+import shutil
+import zipfile
 from cog import BasePredictor, Input, Path
 from PIL import Image
-from rembg import remove
+from rembg import remove, new_session
 import tempfile
-import zipfile
-import os
+import pyheif
+
+u2net = "u2net"
+u2net_human_seg = "u2net_human_seg"
+u2net_cloth_seg = "u2net_cloth_seg"
+u2netp = "u2netp"
 
 
 class Predictor(BasePredictor):
-    def predict(self, input_image_path: Path = Input(description="Upload Single Image Or Zip Folder", default=None)) -> Path:
+    def predict(
+            self, input_image_path: Path = Input(description="Upload single image or zip folder", default=None),
+            model: str = Input(description="select model",
+                               choices=["u2net", "u2net_human_seg", "u2net_cloth_seg", "u2netp"],
+                               default="u2net_human_seg"),
+    ) -> Path:
         if not input_image_path:
             raise ValueError("No input image selected")
 
-        print("Input image path:", input_image_path)
+        # Choose the model
+        selected_model = None
+        if model == "u2net":
+            selected_model = u2net
+        elif model == "u2net_human_seg":
+            selected_model = u2net_human_seg
+        elif model == "u2net_cloth_seg":
+            selected_model = u2net_cloth_seg
+        elif model == "u2netp":
+            selected_model = u2netp
+        else:
+            raise ValueError(f"Invalid model selected: {model}")
 
-        # Create a temporary directory to extract images
-        temp_dir = tempfile.mkdtemp()
+        model_name = selected_model
+        print(f"Using model: {model_name}")
+        session = new_session(model_name)
 
         # Check if the input is a zip file
-        if input_image_path.suffix.lower() == '.zip':
+        if input_image_path.suffix == ".zip":
+            # Unzip the folder
+            unzip_folder = Path(tempfile.mkdtemp())
             with zipfile.ZipFile(input_image_path, 'r') as zip_ref:
-                # Extract all contents of the zip file
-                zip_ref.extractall(temp_dir)
+                zip_ref.extractall(unzip_folder)
 
-            # Create a directory for processed images
-            output_dir = tempfile.mkdtemp()
+            # Process images in the unzipped folder and its subfolders
+            output_folder = Path(tempfile.mkdtemp())
+            self.process_zip_contents(unzip_folder, model_name, output_folder)
 
-            # Process each image in the extracted folder
-            for root, _, files in os.walk(temp_dir):
-                for file_name in files:
-                    file_path = os.path.join(root, file_name)
+            # Zip the output folder
+            result_zip_path = Path(tempfile.mkdtemp()) / "background_removed_images.zip"
+            shutil.make_archive(result_zip_path.with_suffix(""), 'zip', output_folder)
 
-                    if file_path.lower().endswith(('.png', '.jpg', '.jpeg')):
-                        input_image = Image.open(file_path)
+            # Clean up temporary folders
+            shutil.rmtree(unzip_folder)
+            shutil.rmtree(output_folder)
 
-                        print("Processing input image:", file_path)
-
-                        output_image = remove(input_image)
-
-                        # Convert the image to RGB mode
-                        output_image = output_image.convert("RGB")
-
-                        # Save the processed image in the output directory
-                        relative_path = os.path.relpath(file_path, temp_dir)
-                        output_path = os.path.join(output_dir, f"output_{relative_path.replace(os.path.sep, '_')}")
-                        output_image.save(output_path)
-
-                        print("Image processing complete.")
-
-            # Create a zip file containing the processed images
-            output_zip_path = Path(tempfile.mkdtemp()) / "output.zip"
-            with zipfile.ZipFile(output_zip_path, 'w') as zipf:
-                for root, _, files in os.walk(output_dir):
-                    for file_name in files:
-                        file_path = os.path.join(root, file_name)
-                        zipf.write(file_path, arcname=os.path.relpath(file_path, output_dir))
-
-            print("Output images saved at:", output_zip_path)
-
-            return Path(output_zip_path)
-
+            print(f"Result saved to: {result_zip_path}")
+            return result_zip_path
         else:
             # Process a single image
-            input_image = Image.open(input_image_path)
+            output_folder = Path(tempfile.mkdtemp())
+            output_path = self.process_image(input_image_path, model_name, output_folder)
+            print(f"Result saved to: {output_path}")
+            return output_path
 
-            print("Processing input image...")
+    def process_zip_contents(self, folder, model, output_folder):
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                file_path = Path(root) / file
+                self.process_image(file_path, model, output_folder)
 
-            output_image = remove(input_image)
+    def process_image(self, input_image_path, model, output_folder):
+        print(f"Reading input image from: {input_image_path}")
 
-            # Convert the image to RGB mode
-            output_image = output_image.convert("RGB")
+        # Check if the image is a HEIF file
+        if input_image_path.suffix.lower() == ".heif":
+            heif_file = pyheif.read(input_image_path)
+            input_image = Image.frombytes(
+                heif_file.mode,
+                heif_file.size,
+                heif_file.data,
+                "raw",
+                heif_file.mode,
+                heif_file.stride,
+            )
+        else:
+            # Open the image and convert it to RGBA mode (including an alpha channel)
+            input_image = Image.open(input_image_path).convert("RGBA")
 
-            print("Image processing complete.")
+        print("Removing background...")
+        output_image = remove(input_image, session=new_session(model))
 
-            output_path = Path(tempfile.mkdtemp()) / "output.png"
-            output_image.save(output_path)
+        # Save the result in the output folder with a transparent background
+        output_path = output_folder / f"{input_image_path.stem}_background_removed.png"
+        output_image.save(output_path)
 
-            print("Output image saved at:", output_path)
-
-            return Path(output_path)
+        print(f"Result saved to: {output_path}")
+        return output_path
